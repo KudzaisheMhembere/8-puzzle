f6b822e89c6195ca66e8d5057573ad34edffd343
KudzaisheMhembere <74660066+KudzaisheMhembere@users.noreply.github.com>
2021-02-22 22:11:17 +0200

    Update api.py

--- api/api.py
+++ api/api.py
@@ -12,6 +12,40 @@ def get_current_time():
 @app.route('/solution')
 def get_solutions():
     """
-    Generate solution using breath first search
+    Generate solution using breatfirst search
     """
     return jsonify(random.sample(range(9), 9))
+import time
+from flask import Flask, request
+from flask.json import jsonify
+import random
+from api import puzzle
+
+app = Flask(__name__)
+
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+
+@app.route('/initial')
+def get_initial_state():
+    """
+    Generate solution using breath first search
+    """
+    print(jsonify(random.sample(range(9), 9)))
+    return jsonify(random.sample(range(9), 9))
+
+
+@app.route('/solution', methods=['POST'])
+def get_solution():
+    """
+    Generate solution using breath first search
+    """
+    puzzle_board = request.json
+    print(puzzle_board)
+    puzzle_solution = puzzle.get_solution(puzzle_board)
+    print(puzzle_solution)
+    return jsonify(puzzle_solution)
+
